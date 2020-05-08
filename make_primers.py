ac47c903f5c5333ec46876036bda9c094b5cf358
Yi-Pei Chen <52116103+bbchen33@users.noreply.github.com>
2020-05-07 21:47:21 -0700

    Update make_primers.py

--- make_primers.py
+++ make_primers.py
@@ -1,33 +1,31 @@
-def calculate_Tm(input_sequence):
+def calculate_Tm(input_sequence): # simple function to calculate Tm based on the DNA sequence
 	input_sequence = input_sequence.upper()
-	Tm = int(0)
+	Tm = 0
 	for nucleotide in input_sequence:
-		if nucleotide == 'A':
-			Tm += 2
-		elif nucleotide == 'T':
+		if nucleotide in ['A','T']:
 			Tm += 2
 		else:
 			Tm += 4
 	return Tm
 
+import pandas
 def make_primers(input_sequence, length, Tm):
 	input_sequence = input_sequence.upper()
-	input_sequence_list = list(input_sequence)
-	iteration_length = len(input_sequence_list) - length + 1
+	iteration_length = len(input_sequence) - length + 1 # how many times the window size of the assigned primer length will iterate
 	list_of_primer = []
-	for i in range(iteration_length):
-		list_of_primer.append("".join(input_sequence_list[i:i+length]))
-	
 	primer_Tm_dict = {}
-	for primer in list_of_primer:
-		if calculate_Tm(primer) > Tm:
-			primer_Tm_dict[primer] = [calculate_Tm(primer),0]
-	
+	for i in range(iteration_length):
+		primer = input_sequence[i:i+length]
+		if calculate_Tm(primer) >= Tm:
+			primer_Tm_dict[''.join(primer)] = [calculate_Tm(primer), 0]
+
+	if len(primer_Tm_dict) == 0:
+		return "No primers could be found with the given restrains. Try increasing the primer length or lowering the Tm"
+
 	for primer in primer_Tm_dict:
-		primer_Tm_dict[primer][1] = input_sequence.find(primer)
-	
-	import pandas
-	primer_Tm_location_df = pandas.DataFrame(primer_Tm_dict)
-	primer_Tm_location_df = primer_Tm_location_df.transpose()
-	primer_Tm_location_df.columns = ['Tm', 'location',]
+		primer_Tm_dict[primer][1] = input_sequence.find(primer) # find the primer location
+		primer_Tm_location_df = pandas.DataFrame(primer_Tm_dict) # transform the dictionary to pandas dataframe
+		primer_Tm_location_df = primer_Tm_location_df.transpose()
+		primer_Tm_location_df.columns = ['Tm', 'location']
 	return primer_Tm_location_df
+
